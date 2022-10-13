7828d9b6174e06a0ec1968b783c042a3d132e66d
stefanus.suwandi <stefanus.suwandi@binus.edu>
2022-10-13 12:15:54 +0700

    added export playlist using rabbitmq message queue

---
+++ src/api/exports/handler.js
@@ -0,0 +1,33 @@
+class ExportsHandler {
+  constructor(producerService, playlistsService, validator) {
+    this._producerService = producerService;
+    this._playlistsService = playlistsService;
+    this._validator = validator;
+  }
+
+  async postExportPlaylistHandler(request, h) {
+    this._validator.validateExportPlaylistPayload(request.payload);
+    const { id: playlistId } = request.params;
+    const { id: userId } = request.auth.credentials;
+
+    const message = {
+      playlistId,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    await this._playlistsService.verifyPlaylistAccess(playlistId, userId);
+    await this._producerService.sendMessage(
+      "export:playlist",
+      JSON.stringify(message)
+    );
+
+    return h
+      .response({
+        status: "success",
+        message: "Permintaan Anda sedang kami proses",
+      })
+      .code(201);
+  }
+}
+
+module.exports = ExportsHandler;
--- src/server.js
+++ src/server.js
@@ -1,40 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Hapi = require('@hapi/hapi');
-const Jwt = require('@hapi/jwt');
-const config = require('./utils/config');
-const ClientError = require('./exceptions/ClientError');
+const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
+const config = require("./utils/config");
+const ClientError = require("./exceptions/ClientError");
 
 // albums
-const albums = require('./api/albums');
-const AlbumsService = require('./services/postgres/AlbumsService');
-const AlbumValidator = require('./validator/albums');
+const albums = require("./api/albums");
+const AlbumsService = require("./services/postgres/AlbumsService");
+const AlbumValidator = require("./validator/albums");
 
 // songs
-const songs = require('./api/songs');
-const SongsService = require('./services/postgres/SongsService');
-const SongValidator = require('./validator/songs');
+const songs = require("./api/songs");
+const SongsService = require("./services/postgres/SongsService");
+const SongValidator = require("./validator/songs");
 
 // users
-const users = require('./api/users');
-const UsersService = require('./services/postgres/UsersService');
-const UsersValidator = require('./validator/users');
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
 
 // authentications
-const authentications = require('./api/authentications');
-const AuthenticationsService = require('./services/postgres/AuthenticationsService');
-const TokenManager = require('./tokenize/TokenManager');
-const AuthenticationsValidator = require('./validator/authentications');
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
 
 // playlists
-const playlists = require('./api/playlists');
-const PlaylistsService = require('./services/postgres/PlaylistsService');
-const PlaylistsValidator = require('./validator/playlists');
+const playlists = require("./api/playlists");
+const PlaylistsService = require("./services/postgres/PlaylistsService");
+const PlaylistsValidator = require("./validator/playlists");
 
 // collaborations
-const collaborations = require('./api/collaborations');
-const CollaborationsService = require('./services/postgres/CollaborationsService');
-const CollaborationsValidator = require('./validator/collaborations');
+const collaborations = require("./api/collaborations");
+const CollaborationsService = require("./services/postgres/CollaborationsService");
+const CollaborationsValidator = require("./validator/collaborations");
+
+// exports
+const _exports = require("./api/exports");
+const ProducerService = require("./services/rabbitmq/ProducerService");
+const ExportsValidator = require("./validator/exports");
 
 const init = async () => {
   const albumService = new AlbumsService();
@@ -50,7 +55,7 @@ const init = async () => {
     host: config.server.host,
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
@@ -63,7 +68,7 @@ const init = async () => {
   ]);
 
   // define authentication strategy using JWT
-  server.auth.strategy('openmusic_jwt', 'jwt', {
+  server.auth.strategy("openmusic_jwt", "jwt", {
     keys: config.jwt.accessTokenKey,
     verify: {
       aud: false,
@@ -126,15 +131,23 @@ const init = async () => {
         validator: PlaylistsValidator,
       },
     },
+    {
+      plugin: _exports,
+      options: {
+        producerService: ProducerService,
+        playlistsService: playlistsService,
+        validator: ExportsValidator,
+      },
+    },
   ]);
 
   // automatic error handler
-  server.ext('onPreResponse', (request, h) => {
+  server.ext("onPreResponse", (request, h) => {
     const { response } = request;
     if (response instanceof Error) {
       if (response instanceof ClientError) {
         const newResponse = h.response({
-          status: 'fail',
+          status: "fail",
           message: response.message,
         });
         newResponse.code(response.statusCode);
@@ -147,8 +160,8 @@ const init = async () => {
 
       console.log(response);
       const newResponse = h.response({
-        status: 'error',
-        message: 'Oops, something went wrong with the server.',
+        status: "error",
+        message: "Oops, something went wrong with the server.",
       });
       newResponse.code(500);
       return newResponse;
